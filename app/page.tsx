c0f0e967f99cf77de6729715ef29be213ed69eb7
arnaudcrestey <arnaud.crestey14@gmail.com>
2026-03-12 21:32:23 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main style={{
+      display: "flex",
+      height: "100vh",
+      alignItems: "center",
+      justifyContent: "center",
+      fontFamily: "sans-serif"
+    }}>
+      <h1>PERSONALITY SCAN</h1>
+    </main>
+  );
+}
